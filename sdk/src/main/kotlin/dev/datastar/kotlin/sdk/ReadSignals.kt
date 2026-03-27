4fca00974973f7e0150f7226c656da74c7e0e7c5
Ben Croker <57572400+bencroker@users.noreply.github.com>
2026-03-27 12:04:28 -0600

    Handle DELETE method in `ReadSignals`

--- sdk/src/main/kotlin/dev/datastar/kotlin/sdk/ReadSignals.kt
+++ sdk/src/main/kotlin/dev/datastar/kotlin/sdk/ReadSignals.kt
@@ -17,7 +17,7 @@ inline fun <reified T> readSignals(
     request: Request,
     unmarshaller: JsonUnmarshaller<T>,
 ): T {
-    if (request.isGet()) {
+    if (request.method in setOf("GET", "DELETE")) {
         return unmarshaller(request.readParam("datastar"))
     }
     return unmarshaller(request.bodyString())
